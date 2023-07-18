c62a0dfe9a52be3476bfeb7d0c6efb0edee52742
Xiaoyu(Viola) Liu <65391883+XiaoyuLiu198@users.noreply.github.com>
2023-07-17 21:37:42 -0500

    Update parser.py

--- parse_utils/parser.py
+++ parse_utils/parser.py
@@ -7,15 +7,15 @@ PATTERN2PARSER = {SELECT_TYPE: SelectWithPredicatePattern}
 
 class Parser:
     def __init__(self, string: str):
-        self._string = string
-        self._tokenizer = Tokenizer(string)
+        self.string = string
+        self.tokenizer = Tokenizer(string)
 
     def parse(self):
         """
         Match sql operation type then parse into a tree
         :return: query object
         """
-        matched = self._tokenizer.lookahead
+        matched = self.tokenizer.lookahead
         if matched:
             pattern_parser = PATTERN2PARSER[matched.type]
-            return pattern_parser(self._tokenizer).parse()
+            return pattern_parser(self.tokenizer).parse()
